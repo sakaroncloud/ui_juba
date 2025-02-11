cb88b0e4644226c51f1b6b691e87869f0f92d131
Sakar aryal <sakaroncloud@gmail.com>
2025-02-11 16:23:24 +0545

    chore: FormFieldWrapper removed

--- apps/cms/components/choose-image-card/gallery-form.tsx
+++ apps/cms/components/choose-image-card/gallery-form.tsx
@@ -1,18 +1,23 @@
-"use client"
+"use client";
 
 import { useMemo, useState } from "react";
 import { useFormContext } from "react-hook-form";
 
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@repo/ui/components/dialog";
 import { PlusCircleIcon, Upload, X } from "lucide-react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@repo/ui/components/tabs";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@repo/ui/components/tabs";
 
 import { ErrorMessage } from "@hookform/error-message";
 
@@ -22,147 +27,158 @@ import { LibraryContent } from "./library-tab-content";
 
 import { TDefaultImage, TAsyncGallery } from "@repo/ui/types/upload.type";
 import { useLoadMoreFetch } from "@/hooks/useFetch";
+import { Label } from "@repo/ui/components/label";
 
 type Props = {
-    fieldId: string;
-    label: string;
-    imageURL?: string;
-    allowMultiple: boolean | undefined;
-    defaultImages?: TDefaultImage[] | undefined;
-    fetchEndPoint: string;
-    uploadEndPoint: string;
+  fieldId: string;
+  label?: string;
+  imageURL?: string;
+  allowMultiple: boolean | undefined;
+  defaultImages?: TDefaultImage[] | undefined;
+  fetchEndPoint: string;
+  uploadEndPoint: string;
 };
-export const GalleryForm = (
-    {
-        fieldId,
-        label,
-        allowMultiple,
-        fetchEndPoint,
-        uploadEndPoint,
-        defaultImages
-    }: Props
-) => {
+export const GalleryForm = ({
+  fieldId,
+  label,
+  allowMultiple,
+  fetchEndPoint,
+  uploadEndPoint,
+  defaultImages,
+}: Props) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useLoadMoreFetch({
+      pageParam: 1,
+      endPoint: fetchEndPoint,
+      queryKey: fetchEndPoint,
+      take: 10,
+    });
 
-    const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-        useLoadMoreFetch({
-            pageParam: 1,
-            endPoint: fetchEndPoint,
-            queryKey: fetchEndPoint,
-            take: 10,
-        });
+  const gallery: TAsyncGallery = useMemo(
+    () => data?.pages?.flatMap((page: any) => page?.data).filter(Boolean) ?? [],
+    [data]
+  );
+  const form = useFormContext();
+  const [showLibrary, setShowLibrary] = useState(false);
+  const value = form.watch(fieldId);
 
-    const gallery: TAsyncGallery = useMemo(() => data?.pages?.flatMap((page: any) => page?.data).filter(Boolean) ?? [], [data]);
-    const form = useFormContext();
-    const [showLibrary, setShowLibrary] = useState(false);
-    const value = form.watch(fieldId);
+  return (
+    <div className="space-y-2">
+      {label && <Label>{label}</Label>}
+      <Dialog>
+        <DialogTrigger asChild>
+          {/* If value is not empty - no matter either from server or local */}
+          {value && value?.length > 0 ? (
+            allowMultiple ? (
+              // If multiple images are allowed
+              <div className="flex flex-wrap gap-4 bg-white">
+                {value?.map((imageId: string, index: number) => {
+                  const image =
+                    gallery?.find((image) => image?.id === imageId) ||
+                    defaultImages?.find((image) => image?.id === imageId);
 
-    return (
-        <div className="space-y-2">
-            <Dialog>
-                <DialogTrigger asChild>
-                    {/* If value is not empty - no matter either from server or local */}
-                    {value && value?.length > 0 ? (
-                        allowMultiple ? (
-                            // If multiple images are allowed
-                            <div className="flex flex-wrap gap-4">
-                                {value?.map((imageId: string, index: number) => {
-                                    const image = gallery?.find(
-                                        (image) => image?.id === imageId
-                                    ) || defaultImages?.find((image) => image?.id === imageId);
-
-                                    if (image?.url) {
-                                        return (
-                                            <div className="w-fit relative" key={index}>
-                                                <FallbackImage
-                                                    src={image.url}
-                                                    alt="image"
-                                                    type="rectangle"
-                                                    height={200}
-                                                    width={200}
-                                                    className="rounded object-cover max-h-[160px] w-auto"
-                                                />
-                                                <div className="absolute inset-0 hover:bg-gray-800/20 transition-all group duration-300 ease-in h-full w-full cursor-pointer flex items-center justify-center">
-                                                    <PlusCircleIcon className="text-white size-8 invisible group-hover:visible transition-all duration-300 ease-in opacity-0 group-hover:opacity-100" />
-                                                </div>
-                                            </div>
-                                        );
-                                    }
-                                })}
-                            </div>
-                        ) : (
-                            // for single image
-                            <div className="w-fit relative">
-                                <FallbackImage
-                                    src={
-                                        (gallery?.find((image) => image?.id === value)?.url ||
-                                            defaultImages?.find((image) => image?.id === value)?.url || "/")
-                                    }
-                                    alt="image"
-                                    type="rectangle"
-                                    height={200}
-                                    width={200}
-                                    className="rounded object-cover max-h-[160px] w-auto"
-                                />
-                                <div className="absolute inset-0 hover:bg-gray-800/20 transition-all group duration-300 ease-in h-full w-full cursor-pointer flex items-center justify-center">
-                                    <PlusCircleIcon className="text-white size-8 invisible group-hover:visible transition-all duration-300 ease-in opacity-0 group-hover:opacity-100" />
-                                </div>
-                            </div>
-                        )
-                    ) : (
-                        <div className="border flex items-center justify-center border-dashed cursor-pointer size-20 rounded-xl">
-                            <Upload className="text-gray-500 size-5" />
+                  if (image?.url) {
+                    return (
+                      <div className="w-fit relative" key={index}>
+                        <FallbackImage
+                          src={image.url}
+                          alt="image"
+                          type="rectangle"
+                          height={200}
+                          width={200}
+                          className="rounded object-cover max-h-[160px] w-auto"
+                        />
+                        <div className="absolute inset-0 hover:bg-gray-800/20 transition-all group duration-300 ease-in h-full w-full cursor-pointer flex items-center justify-center">
+                          <PlusCircleIcon className="text-white size-8 invisible group-hover:visible transition-all duration-300 ease-in opacity-0 group-hover:opacity-100" />
                         </div>
-                    )}
-                </DialogTrigger>
-                <DialogContent className="max-w-[96vw] h-[96vh] w-full flex flex-col justify-between overflow-y-scroll">
-                    <DialogHeader>
-                        <DialogTitle className="flex justify-between items-center border-b pb-3 h-10">
-                            {label}
-                            <DialogClose>
-                                <X className="text-black" />
-                            </DialogClose>
-                        </DialogTitle>
-                    </DialogHeader>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            ) : (
+              // for single image
+              <div className="relative bg-white border border-dotted p-4">
+                <FallbackImage
+                  src={
+                    gallery?.find((image) => image?.id === value)?.url ||
+                    defaultImages?.find((image) => image?.id === value)?.url ||
+                    "/"
+                  }
+                  alt="image"
+                  type="rectangle"
+                  height={200}
+                  width={200}
+                  className="rounded object-cover max-h-[160px] w-auto"
+                />
+                <div className="absolute inset-0 hover:bg-gray-800/20 transition-all group duration-300 ease-in h-full w-full cursor-pointer flex items-center justify-center">
+                  <PlusCircleIcon className="text-white size-8 invisible group-hover:visible transition-all duration-300 ease-in opacity-0 group-hover:opacity-100" />
+                </div>
+              </div>
+            )
+          ) : (
+            <div className="border bg-white cursor-pointer flex items-center p-6 py-8 flex-col justify-center border-dashed  rounded-xl">
+              <p className="">
+                <Upload />
+              </p>
+              <p className="text-sm">Click to Upload</p>
+            </div>
+          )}
+        </DialogTrigger>
+        <DialogContent className="max-w-[96vw] h-[96vh] w-full flex flex-col justify-between overflow-y-scroll">
+          <DialogHeader>
+            <DialogTitle className="flex justify-between items-center border-b pb-3 h-10">
+              {label}
+              <DialogClose>
+                <X className="text-black" />
+              </DialogClose>
+            </DialogTitle>
+          </DialogHeader>
 
-                    <div className="w-full flex-1 flex justify-between  h-[(calc(96vh-80px))] overflow-y-auto">
-                        <Tabs
-                            onValueChange={(e: string) => {
-                                setShowLibrary(e === "library");
-                            }}
-                            value={showLibrary ? "library" : "upload"}
-                            className="w-full"
-                        >
-                            <TabsList className="grid w-full grid-cols-2">
-                                <TabsTrigger value="upload">Upload</TabsTrigger>
-                                <TabsTrigger value="library">Media Library</TabsTrigger>
-                            </TabsList>
-                            <TabsContent value="upload">
-                                {/* Upload Content */}
-                                <AsyncDropZone setShowLibrary={setShowLibrary} endPoint={uploadEndPoint} />
-                            </TabsContent>
-                            <TabsContent value="library">
-                                <LibraryContent
-                                    label={label} gallery={gallery} fieldId={fieldId} value={value}
-                                    hasNextPage={hasNextPage} isFetchingNextPage={isFetchingNextPage}
-                                    fetchNextPage={fetchNextPage}
-                                    allowMultiple={allowMultiple}
-                                />
-                            </TabsContent>
-                        </Tabs>
-                    </div>
-                </DialogContent>
-            </Dialog>
-            <ErrorMessage
-                errors={form.formState.errors}
-                name={fieldId}
-                render={({ message }) => (
-                    <p className="text-[0.8rem] font-medium text-destructive">
-                        {message}
-                    </p>
-                )}
-            />
-        </div>
-    )
-}
-
-
+          <div className="w-full flex-1 flex justify-between  h-[(calc(96vh-80px))] overflow-y-auto">
+            <Tabs
+              onValueChange={(e: string) => {
+                setShowLibrary(e === "library");
+              }}
+              value={showLibrary ? "library" : "upload"}
+              className="w-full"
+            >
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="upload">Upload</TabsTrigger>
+                <TabsTrigger value="library">Media Library</TabsTrigger>
+              </TabsList>
+              <TabsContent value="upload">
+                {/* Upload Content */}
+                <AsyncDropZone
+                  setShowLibrary={setShowLibrary}
+                  endPoint={uploadEndPoint}
+                />
+              </TabsContent>
+              <TabsContent value="library">
+                <LibraryContent
+                  label={label}
+                  gallery={gallery}
+                  fieldId={fieldId}
+                  value={value}
+                  hasNextPage={hasNextPage}
+                  isFetchingNextPage={isFetchingNextPage}
+                  fetchNextPage={fetchNextPage}
+                  allowMultiple={allowMultiple}
+                />
+              </TabsContent>
+            </Tabs>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <ErrorMessage
+        errors={form.formState.errors}
+        name={fieldId}
+        render={({ message }) => (
+          <p className="text-[0.8rem] font-medium text-destructive">
+            {message}
+          </p>
+        )}
+      />
+    </div>
+  );
+};
--- apps/cms/features/lodging/form/property-locations-form.tsx
+++ apps/cms/features/lodging/form/property-locations-form.tsx
@@ -1,9 +1,13 @@
 import { CustomFormField } from "@/components/form/custom-form-field";
-import { FormFieldWrapper, FormFooter } from "@/components/form/form-field-wrapper";
+import { FormFooter } from "@/components/form/form-field-wrapper";
 import { Button } from "@repo/ui/components/button";
 import { Form } from "@repo/ui/components/form";
 import { submitPropertyLocations } from "@/lib/actions/lodging/action.property";
-import { defaultPropertyLocations, propertyLocationsSchema, TPropertyLocationsForm } from "@repo/ui/schemas/lodging/property/property-locations.schema";
+import {
+  defaultPropertyLocations,
+  propertyLocationsSchema,
+  TPropertyLocationsForm,
+} from "@repo/ui/schemas/lodging/property/property-locations.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
@@ -11,95 +15,92 @@ import toast from "react-hot-toast";
 import { handleToast } from "@repo/ui/lib/utils";
 
 type Props = {
-    formValues?: TPropertyLocationsForm;
-    id: string;
-}
+  formValues?: TPropertyLocationsForm;
+  id: string;
+};
 export const PropertyLocationsForm = ({ formValues, id }: Props) => {
+  const form = useForm<TPropertyLocationsForm>({
+    resolver: zodResolver(propertyLocationsSchema),
+    defaultValues: formValues || defaultPropertyLocations,
+    mode: "onChange",
+  });
 
-    const form = useForm<TPropertyLocationsForm>({
-        resolver: zodResolver(propertyLocationsSchema),
-        defaultValues: formValues || defaultPropertyLocations,
-        mode: "onChange",
-    })
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "places",
+  });
 
-    const { fields, append, remove } = useFieldArray({
-        control: form.control,
-        name: "places"
-    })
+  const [isPending, startTransition] = useTransition();
 
-    const [isPending, startTransition] = useTransition();
+  const onSubmit = (values: TPropertyLocationsForm) => {
+    startTransition(async () => {
+      const response = await submitPropertyLocations(values, id);
+      handleToast(response);
+    });
+  };
 
-    const onSubmit = (values: TPropertyLocationsForm) => {
-        startTransition(async () => {
-            const response = await submitPropertyLocations(values, id);
-            handleToast(response)
-        })
-    };
+  const isValid = form.formState.isValid;
 
-    const isValid = form.formState.isValid
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-6"
+      >
+        <div className="w-full space-y-5">
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex gap-4">
+              <CustomFormField
+                elementName="input"
+                fieldId={`places.${index}.name`}
+                label="Name"
+                placeholder="Enter name"
+                className="w-full"
+              />
+              <CustomFormField
+                elementName="input"
+                inputType="number"
+                fieldId={`places.${index}.distance`}
+                label="Distance"
+                placeholder="Enter distance in meters"
+                className="w-full"
+              />
+              <div className="w-12 flex self-end">
+                {fields.length > 1 && (
+                  <Button
+                    className="self-end h-8 w-8 text-red-500"
+                    variant="outline"
+                    size={"lg"}
+                    onClick={() => remove(index)}
+                  >
+                    Delete
+                  </Button>
+                )}
+              </div>
+            </div>
+          ))}
 
+          {isValid && (
+            <Button
+              type="button"
+              variant="outline"
+              size="sm"
+              className="mt-2"
+              onClick={() => append({ name: "", distance: 0 })}
+            >
+              Add More
+            </Button>
+          )}
+        </div>
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6">
-                <FormFieldWrapper
-                    description='Select all general rules  that are applicable'
-                    label='Nearest Transportations Accessibility'
-                    className='flex justify-between flex-1 '
-                >
-
-                    <div className="w-full space-y-5">
-                        {fields.map((field, index) => (
-                            <div key={field.id} className="flex gap-4">
-                                <CustomFormField
-                                    elementName="input"
-                                    fieldId={`places.${index}.name`}
-                                    label="Name"
-                                    placeholder="Enter name"
-                                    className="w-full"
-                                />
-                                <CustomFormField
-                                    elementName="input"
-                                    inputType="number"
-                                    fieldId={`places.${index}.distance`}
-                                    label="Distance"
-                                    placeholder="Enter distance in meters"
-                                    className="w-full"
-                                />
-                                <div className="w-12 flex self-end">
-                                    {fields.length > 1 && <Button
-                                        className="self-end h-8 w-8 text-red-500"
-                                        variant="outline"
-                                        size={"lg"}
-                                        onClick={() => remove(index)}
-                                    >
-                                        Delete
-                                    </Button>}
-                                </div>
-                            </div>
-                        ))}
-
-                        {isValid && <Button
-                            type="button"
-                            variant="outline"
-                            size="sm"
-                            className="mt-2"
-                            onClick={() => append({ name: "", distance: 0 })}
-                        >
-                            Add More
-                        </Button>}
-                    </div>
-                </FormFieldWrapper>
-
-
-                <FormFooter
-                    buttonLabel={"Update"}
-                    pending={isPending}
-                    goBack={{
-                        path: `/properties`
-                    }}
-                />
-            </form>
-        </Form>
-    )
-}
+        <FormFooter
+          buttonLabel={"Update"}
+          pending={isPending}
+          goBack={{
+            path: `/properties`,
+          }}
+        />
+      </form>
+    </Form>
+  );
+};
